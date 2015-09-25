fe77ce9233e7293b4fa75f79e9115e13b22cbf7c
Araik Tamazian <araik.tamazian@outlook.com>
2015-09-25 15:46:29 +0300

    Code improvements

--- usask_session.R
+++ usask_session.R
@@ -1,27 +1,36 @@
+# This script extracts user session data using University of Saskatchewan WWW server log
+# taken from The Internet Traffic Archive (http://ita.ee.lbl.gov)
 
+# Read log ------------------------------------------------------------------------------
 infname <- "UofS_access_log.txt"
 hlog <- read.table(infname, fill = TRUE, stringsAsFactors = FALSE)
-hlog <- hlog[,c(1,4,8)]
+# Select only host, time and size fields
+hlog <- hlog[, c(1, 4, 8)]
 colnames(hlog)<- c("host", "time", "size")
+# Eliminate NAs
 hlog$size <- replace(hlog$size, hlog$size == '-', NA)
 hlog$size <- replace(hlog$size, hlog$size == 0, NA)
 hlog$time <- substring(hlog$time,2)
-
+# Date conversion
 hlog$time <- as.POSIXct(hlog$time, format="%d/%B/%Y:%H:%M:%S")
+# Get interarrival time
 hlog$time <- c(0,diff(as.numeric(hlog$time)))
+# Eliminate NAs and convert to numeric
 hlog <- hlog[complete.cases(hlog), ]
 hlog$size <- as.numeric(hlog$size)
 
-max.ts <- 30
-k <- 1 #number session
-sum_packet <-0  #total rows to final session 
-t_session <- 0
-size_session <- 0 #size of session
+# User session extraction ---------------------------------------------------------------------------
+max.ts <- 30                # maximum time between packets allowed in session
+k <- 1                      # session number
+sum_packet <-0              # total rows to final session 
+t_session <- 0              # time between sessions (in seconds)
+size_session <- 0           # session total size (in bytes)
 size_paket <- 0
-number_paketinsession <- 0 #number paket in session 
+number_paketinsession <- 0  # number of connections in session 
 number_paket <- 0
 n<- 11
 
+# Main loop
 for(i in 2:n) {
   number_paket <- number_paket +1
   size_paket <- size_paket +hlog$size[i-1]
@@ -40,20 +49,17 @@ for(i in 2:n) {
   }
 }
 
-#------------find parameters size and "number paket in session " for final sesssion -------#
+# Find session size and number of connections for final sesssion
 num_diff <- n - sum_packet 
 number_paketinsession[k] <- num_diff
 size_session[k] <- 0
-for (i in 1:num_diff)
-{
+for (i in 1:num_diff) {
   size_session[k] <- size_session[k] + hlog$size[sum_packet+i]
 }
-#--------------------------------------------------------------------------------------------
 
-
+# Assemble results in dataframe
 size_session <- size_session[2:length(size_session)]
 number_paketinsession <- number_paketinsession[2:length(number_paketinsession)]
- 
 ses <- data.frame(time = t_session, ncon = number_paketinsession, size = size_session)
 
 x <- ses$time
