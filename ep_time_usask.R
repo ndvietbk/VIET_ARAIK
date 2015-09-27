9edf828524e773c9e17fe58f54ba3b16482748d3
ndvietleti <ndvietleti@gmail.com>
2015-09-27 23:22:22 +0300

    Update ep_time_usask.R

--- ep_time_usask.R
+++ ep_time_usask.R
@@ -7,7 +7,7 @@ library(sfsmisc)
 data <- read.table("D:/dorm2/20150921/session_extract_usask.txt") # read data from file session_extract_usask.txt
 colnames(data) <- c("time","num_connection", "ssize") # Assign names for the column
 
-Fn <- data$time
+Fn <- ecdf(data$time)
 x <- seq(1,100,length.out = 100)
 ep <- 1-Fn(x)
 out <- data.frame(x=x, ep=ep)
