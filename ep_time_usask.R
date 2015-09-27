79a163f8700cc32661ab744bc07d57b4af086559
ndvietleti <ndvietleti@gmail.com>
2015-09-27 23:35:46 +0300

    Update ep_time_usask.R

--- ep_time_usask.R
+++ ep_time_usask.R
@@ -8,7 +8,7 @@ data <- read.table("D:/dorm2/20150921/session_extract_usask.txt") # read data fr
 colnames(data) <- c("time","num_connection", "ssize") # Assign names for the column
 
 Fn <- ecdf(data$time)
-x <- seq(1,100,length.out = 100)
+x <- lseq(7.96e-2, 1000,length = 100)
 ep <- 1-Fn(x)
 out <- data.frame(x=x, ep=ep)
 write.table(out, "ep_time_session_usk.txt", row.names = F, col.names = F)
