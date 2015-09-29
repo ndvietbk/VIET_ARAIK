1194ed6673638b7bb752cb2aa73dfd1a9d931638
Araik Tamazian <araik.tamazian@outlook.com>
2015-09-29 12:54:00 +0300

    Update ep_time_mix.R

--- ep_time_mix.R
+++ ep_time_mix.R
@@ -3,21 +3,24 @@ library(reliaR)
 n <- 1e+5
 alpha <- 0.33
 lambda <- 20
-data1 <- rexp.ext(n, alpha, lambda)
-data2 <- rexp.ext(n, alpha, lambda)
-data3 <- rexp.ext(n, alpha, lambda)
-data4 <- rexp.ext(n, alpha, lambda)
+nthr <- 2
 
-data <- cbind(matrix(data1), matrix(data2), matrix(data3), matrix(data4))
-
-# main loop for swap data$V1,...,data$V4
-for (i in 1:n) {
+# Generate threads
+for (i in 1:nthr) {
+  one.thr <- rexp.ext(n, alpha, lambda)
   if (i == 1) {
-    x <- 0
+    all.thr <- one.thr
+  } else {
+    all.thr <- cbind(all.thr, matrix(one.thr))
   }
-  j <- sample(1:4,4,replace = F) #generate random number in 1:4
-  k <- 0
-  x <- c(x, data[i,j])
 }
 
-x <- x[2:length(x)]
+# Combine threads to one thread using random mix
+for (i in 1:n) {
+  if (i == 1) {
+    comb.thr <- 0
+  }
+  j <- sample(1:4, 4) #generate random number in 1:4
+  comb.thr <- c(comb.thr, all.thr[i, j])
+}
+comb.thr <- x[2:length(comb.thr)]
