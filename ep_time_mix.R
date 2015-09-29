8297478f8a1434a724cfe1fbfa319a001c802bf8
Araik Tamazian <araik.tamazian@outlook.com>
2015-09-29 12:49:10 +0300

    Update ep_time_mix.R

--- ep_time_mix.R
+++ ep_time_mix.R
@@ -1,33 +1,23 @@
-library(minpack.lm)
 library(reliaR)
-library(sfsmisc)
 
 n <- 1e+5
 alpha <- 0.33
 lambda <- 20
 data1 <- rexp.ext(n, alpha, lambda)
-data2 <- read.table("D:/dorm2/20150921/ep_time/ep_time_20150417D.txt")
-data3 <- read.table("D:/dorm2/20150921/ep_time/ep_time_20150418D.txt")
-data4 <- read.table("D:/dorm2/20150921/ep_time/ep_time_20150419D.txt")
+data2 <- rexp.ext(n, alpha, lambda)
+data3 <- rexp.ext(n, alpha, lambda)
+data4 <- rexp.ext(n, alpha, lambda)
 
-data1$V1 <- NULL
-data2$V1 <- NULL
-data3$V1 <- NULL
-data4$V1 <- NULL
-data1 <- head(data1,10000)  
-data2 <- head(data2,10000)
-data3 <- head(data3,10000)
-data4 <- head(data4,10000)
-
-data <- cbind(data1, data2, data3, data4)
-colnames(data) <- c("V1", "V2", "V3", "V4")
+data <- cbind(matrix(data1), matrix(data2), matrix(data3), matrix(data4))
 
 # main loop for swap data$V1,...,data$V4
-for (i in 1:10000) {
-  n <- sample(1:4,4,replace = F) #generate random number in 1:4
-  data_temp <- data[i,]
-  data[i, ]$V1 <- data_temp[n[1]] 
-  data[i, ]$V2 <- data_temp[n[2]]
-  data[i, ]$V3 <- data_temp[n[3]]
-  data[i, ]$V4 <- data_temp[n[4]]
+for (i in 1:n) {
+  if (i == 1) {
+    x <- 0
+  }
+  j <- sample(1:4,4,replace = F) #generate random number in 1:4
+  k <- 0
+  x <- c(x, data[i,j])
 }
+
+x <- x[2:length(x)]
